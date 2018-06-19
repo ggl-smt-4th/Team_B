a6853d139d1e033be1116c585be4682d58583ab9
yunjiami <39847631+yunjiami@users.noreply.github.com>
2018-06-19 14:13:53 +0800

    范春 作业

--- orgin/payroll.sol
+++ orgin/payroll.sol
@@ -1,49 +1,42 @@
 pragma solidity ^0.4.14;
 
 contract Payroll {
-    uint constant payDuration = 10 seconds;
-
-    address owner;
+    uint x;
     uint salary;
-    address employee;
-    uint lastPayday;
-
-    function Payroll() {
-        owner = msg.sender;
+    address y;
+    address frank;
+    uint constant payDuration = 10 seconds;
+    uint lastPayday = now ;
+    
+    function set(uint x) {
+      salary = x * 1 ether;
     }
     
-    function updateEmployee(address e, uint s) {
-        require(msg.sender == owner);
-
-        if (employee != 0x0) {
-            uint payment = salary * (now - lastPayday) / payDuration;
-            employee.transfer(payment);
-        }
-
-        employee = e;
-        salary = s * 1 ether;
-        lastPayday = now;
+    function set(address y){
+      frank = y;
     }
     
-    function addFund() payable returns (uint) {
+    function addFund() payable returns(uint) {
         return this.balance;
     }
     
-    function calculateRunway() returns (uint) {
+    function calculateRunway()returns (uint) {
         return this.balance / salary;
     }
     
-    function hasEnoughFund() returns (bool) {
+    function hasenoughFund() returns (bool) {
         return calculateRunway() > 0;
     }
     
     function getPaid() {
-        require(msg.sender == employee);
-        
+        if (msg.sender != frank) {
+            revert();
+        }
         uint nextPayday = lastPayday + payDuration;
-        assert(nextPayday < now);
-
-        lastPayday = nextPayday;
-        employee.transfer(salary);
+        if (nextPayday > now){
+            revert();
+        }
+            lastPayday = nextPayday;
+            frank.transfer(salary);
     }
 }

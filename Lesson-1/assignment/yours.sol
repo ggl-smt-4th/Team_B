d9c18a7e11ef3c9325469e22d29cf4a397d3e490
Bo Yao <byao@mymakecents.com>
2018-06-19 11:40:15 -0400

    homework 1

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,60 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint salary = 1 ether;
+    address owner;
+    address employee = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+    uint constant payDuration = 10 seconds;
+    uint lastPayday = now;
+
+    function Payroll() {
+        owner = msg.sender;
+    }
+
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+
+    function getSalary() returns (uint) {
+        return salary;
+    }
+
+    function getEmployeeAddress() returns (address) {
+        return employee;
+    }
+
+    function updateEmployeeAddress(address e) {
+        require(msg.sender == owner);
+
+        employee = e;
+    }
+
+    function updateEmployeeSalary(uint s) {
+        require(msg.sender == owner);
+
+        salary = s * 1 ether;
+    }
+
+
+    function getPaid() {
+        if (msg.sender != employee) {
+            revert();
+        }
+
+        uint nextPayDay = lastPayday + payDuration;
+        if (nextPayDay > now) {
+            revert();
+        }
+
+        lastPayday = nextPayDay;
+        employee.transfer(salary);
+    }
+}

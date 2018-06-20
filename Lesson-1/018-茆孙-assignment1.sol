70caf41ae7e36a91217d6d5ef0d075f01696bbfc
ms940416 <sm4206@columbia.edu>
2018-06-19 23:06:16 -0700

    Update and rename Lesson-1/assignment/yours.sol to Lesson-1/018-茆孙-assignment1.sol

---
+++ Lesson-1/018-茆孙-assignment1.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.4.13;
+
+contract Payroll {
+    uint constant payDuration = 10 seconds;
+
+    address owner;
+    uint salary;
+    address employee;
+    uint lastPayday;
+
+    function Payroll() {
+        owner = msg.sender;
+    }
+
+    function updateEmployee(address e, uint s) {
+        require(msg.sender == owner);
+
+        if (employee != 0x0) {
+            uint payment = salary * (now - lastPayday) / payDuration;
+            employee.transfer(payment);
+        }
+
+        employee = e;
+        salary = s * 1 ether;
+        lastPayday = now;
+    }
+
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+
+    function getPaid() {
+        require(msg.sender == employee);
+
+        uint nextPayday = lastPayday + payDuration;
+        assert(nextPayday < now);
+
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+    }
+    
+    function changeEmployeeSalary(uint newSalary){
+        require(msg.sender == owner);
+        salary = newSalary * 1 ether;
+    }
+    
+    function changeEmployeeAddress(address newAddress) {
+        require(msg.sender == owner);
+        employee = newAddress;
+    }
+    
+}

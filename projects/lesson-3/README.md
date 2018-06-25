7cb35e2e18584e24a1de97cb6dbae6d00c2e8277
alexygui <guigen.wang.cn@gmail.com>
2018-06-25 21:37:16 +0800

    第三课作业

--- projects/lesson-3/README.md
+++ projects/lesson-3/README.md
@@ -20,4 +20,54 @@
     contract Z is K1, K2
     ```
 
-对于非 Windows 用户，安装 docker 后，运行 `sh manager.sh run_test` 可测试合约是否满足基本要求。
+对于非 Windows 用户，安装 docker 后，运行 `sh manager.sh run_tests` 可测试合约是否满足基本要求。
+
+---
+
+2.已经有“员工存在”的修饰语，同理，也可以有“员工不存在”的修饰语。虽然目前没有其他函数复用这个修饰语，但是依然可以提取出来。
+
+3.在C3 Linearization中，merge操作是C3算法的核心。根据网上资料显示，遍历执行merge操作的序列，如果一个序列的第一个元素，是其他序列中的第一个元素，或不在其他序列出现，则从所有执行merge操作序列中删除这个元素，合并到当前的L中。
+
+由此可得：
+
+L(O) := [O]
+
+L(A) := [A] + merge(L(O) + [O])
+
+​	= [A] + merge([O] + [O])
+
+​	= [A, O]
+
+L(B) := [B, O]
+
+L(C) := [C, O]
+
+L(K1) := [K1] + merge(L(A) + L(B) + [A, B])
+
+​	= [K1] + merge([A, O] + [B, O] + [A, B])
+
+​	= [K1, A] + merge([O] + [B, O] + [B])
+
+​	= [K1, A, B] + merge([O] + [O])
+
+​	= [K1, A, B, O]
+
+L(K2) := [K2] + merge(L(A) + L(C) + [A, C])
+
+​	= [K2] + merge([A, O] + [C, O] + [A, C])
+
+​	= [K2, A, C, O]
+
+L(Z) := [Z] + merge(L(K1) + L(K2) + [K1, K2])
+
+​	= [Z] + merge([K1, A, B, O] + [K2, A, C, O] + [K1, K2])
+
+​	= [Z, K1] + merge([A, B, O] + [K2, A, C, O] + [K2])
+
+​	= [Z, K1, K2] + merge([A, B, O] + [A, C, O])
+
+​	= [Z, K1, K2, A] + merge([B, O] + [C, O])
+
+​	= [Z, K1, K2, A, B] + merge([O] + [C, O])
+
+​	= [Z, K1, K2, A, B, C, O] 
---
+++ projects/lesson-3/contracts/Payroll.sol
@@ -0,0 +1,104 @@
+pragma solidity ^0.4.14;
+
+
+import './SafeMath.sol';
+import './Ownable.sol';
+
+
+contract Payroll is Ownable {
+
+    using SafeMath for uint;
+
+    struct Employee {
+        address id;
+        uint salary;
+        uint lastPayday;
+    }
+
+    //所有员工的mapping
+    mapping (address => Employee) public employees;
+
+    // uint constant payDuration = 10 seconds;    //test
+    //定值，支付薪水的时间间隔
+    uint constant payDuration = 30 days;
+
+    //员工合计的工资数，在add、update、remove的时候进行修改，优化calculateRunway方法调用gas消耗
+    uint public totalSalary = 0;
+
+    //内部函数，清算支付某员工之前所有的薪水
+    function _patialPaied(Employee employee) private {
+        var payment = employee.salary.mul((now.sub(employee.lastPayday))).div(payDuration);
+        employee.id.transfer(payment);
+    }
+
+    //修饰语，员工存在于公司的员工的mapping
+    modifier employeeExist(address employeeId) {
+        var employee = employees[employeeId];
+        assert(employee.id != 0x0);
+        _;
+    }
+
+    //修饰语，员工不存在于公司的员工的mapping
+    modifier employeeNotExist(address employeeId) {
+        var employee = employees[employeeId];
+        assert(employee.id == 0x0);
+        _;
+    }
+
+    //向员工mapping中添加员工的地址、薪水等信息
+    function addEmployee(address employeeId, uint salary) public onlyOwner employeeNotExist(employeeId) {
+        employees[employeeId] = Employee(employeeId, salary.mul(1 ether), now);
+        totalSalary = totalSalary.add(salary.mul(1 ether));
+    }
+
+    //从员工mapping中删除某个ID的员工
+    function removeEmployee(address employeeId) public onlyOwner employeeExist(employeeId) {
+        var employee = employees[employeeId];
+        _patialPaied(employee);
+        totalSalary = totalSalary.sub(employee.salary);
+        delete employees[employeeId];
+    }
+
+    //修改员工接收薪水的地址，仅限合约创建者修改
+    function changePaymentAddress(address oldAddress, address newAddress) public onlyOwner employeeExist(oldAddress) {
+        var employee = employees[oldAddress];
+        employees[newAddress] = Employee(newAddress, employee.salary, employee.lastPayday);
+        delete employees[oldAddress];
+    }
+
+    //更新某员工的数据，根据ID（地址）更新薪水和上次支付时间，需要先根据以前的工资水平清算支付以前的工资
+    function updateEmployee(address employeeId, uint salary) public onlyOwner employeeExist(employeeId) {
+        var employee = employees[employeeId];
+
+        totalSalary = totalSalary.add((salary.mul(1 ether)).sub(employee.salary));
+        employees[employeeId].salary = salary.mul(1 ether);
+        _patialPaied(employee);
+        employees[employeeId].lastPayday = now;
+    }
+
+    //设置value，通过外部账户向合约账户中添加ether
+    function addFund() payable public returns (uint) {
+        return address(this).balance;
+    }
+
+    //计算合约地址中的ether还可以支付所有员工多少次工资
+    function calculateRunway() public view returns (uint) {
+        return address(this).balance.div(totalSalary);
+    }
+
+    //合约地址中是否有足够的钱支付员工工资
+    function hasEnoughFund() public view returns (bool) {
+        return calculateRunway() > 0;
+    }
+
+    //根据当前登录的员工的地址判断是否可以支付，以及支付该员工相对应数量ether的工资
+    function getPaid() public employeeExist(msg.sender) {
+        var employee = employees[msg.sender];
+
+        uint nextPayday = employee.lastPayday.add(payDuration);
+
+        assert(nextPayday < now);
+        employee.lastPayday = nextPayday;
+        employee.id.transfer(employee.salary);
+    }
+}

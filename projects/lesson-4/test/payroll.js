d3fdf2806092fde04ceac5d51f7c12604ea70483
alexygui <guigen.wang.cn@gmail.com>
2018-07-01 10:27:47 +0800

    添加getPai.js

--- projects/lesson-4/test/payroll.js
+++ projects/lesson-4/test/payroll.js
@@ -6,8 +6,6 @@ contract('Payroll', function (accounts) {
     const guest = accounts[2];
     const salary = 1;
     const runway = 20;
-    const payDuration = 60 * 60 * 24 * 30; // 30 days
-    const fund = runway * salary;
 
 
     it("Test call addEmployee() by owner", function () {
@@ -59,63 +57,6 @@ contract('Payroll', function (accounts) {
             assert(false, "Should not be successful");
         }).catch(error => {
             assert.include(error.toString(), "Error: VM Exception", "Cannot call removeEmployee() by guest");
-        });
-    });
-
-
-    it("Test getPaid()", function () {
-        var payroll;
-        return Payroll.new.call(owner, { from: owner, value: web3.toWei(fund, 'ether') }).then(instance => {
-            payroll = instance;
-            return payroll.addEmployee(employee, salary, { from: owner });
-        }).then(() => {
-            return payroll.calculateRunway();
-        }).then(runwayRet => {
-            if (!runwayRet.toNumber || typeof runwayRet.toNumber !== "function") {
-                assert(false, "the function `calculateRunway()` should be defined as: `function calculateRunway() public view returns (uint)` | `calculateRunway()` 应定义为: `function calculateRunway() public view returns (uint)`");
-            }
-            assert.equal(runwayRet.toNumber(), runway, "Runway is wrong");
-            return web3.currentProvider.send({ jsonrpc: "2.0", method: "evm_increaseTime", params: [payDuration], id: 0 });
-        }).then(() => {
-            return payroll.getPaid({ from: employee })
-        }).then((getPaidRet) => {
-            return payroll.calculateRunway();
-        }).then(runwayRet => {
-            assert.equal(runwayRet.toNumber(), runway - 1, "The runway is not correct");
-        });
-    });
-
-    it("Test getPaid() before duration", function () {
-        var payroll;
-        return Payroll.new.call(owner, { from: owner, value: web3.toWei(fund, 'ether') }).then(instance => {
-            payroll = instance;
-            return payroll.addEmployee(employee, salary, { from: owner });
-        }).then(() => {
-            return payroll.calculateRunway();
-        }).then(runwayRet => {
-            assert.equal(runwayRet.toNumber(), runway, "Runway is wrong");
-            return payroll.getPaid({ from: employee })
-        }).then((getPaidRet) => {
-            assert(false, "Should not be successful");
-        }).catch(error => {
-            assert.include(error.toString(), "Error: VM Exception", "Should not getPaid() before a pay duration");
-        });
-    });
-
-    it("Test getPaid() by a non-employee", function () {
-        var payroll;
-        return Payroll.new.call(owner, { from: owner, value: web3.toWei(fund, 'ether') }).then(instance => {
-            payroll = instance;
-            return payroll.addEmployee(employee, salary, { from: owner });
-        }).then(() => {
-            return payroll.calculateRunway();
-        }).then(runwayRet => {
-            assert.equal(runwayRet.toNumber(), runway, "Runway is wrong");
-            return payroll.getPaid({ from: guest })
-        }).then((getPaidRet) => {
-            assert(false, "Should not be successful");
-        }).catch(error => {
-            assert.include(error.toString(), "Error: VM Exception", "Should not call getPaid() by a non-employee");
         });
     });
 

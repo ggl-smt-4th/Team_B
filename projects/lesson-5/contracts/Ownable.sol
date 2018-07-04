7558738e6b4dba520a8308e2cd85184a30f45a1d
Bob <wngbob@qq.com>
2018-07-04 13:15:07 +0800

    Update Ownable.sol

--- projects/lesson-5/contracts/Ownable.sol
+++ projects/lesson-5/contracts/Ownable.sol
@@ -17,7 +17,7 @@ contract Ownable {
      * @dev The Ownable constructor sets the original `owner` of the contract to the sender
      * account.
      */
-    function constructor() public {
+    function Ownable() public {
         owner = msg.sender;
     }
 
